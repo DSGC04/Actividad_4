7ca088888ef4955902194c1dcad6876e5dadda5c
ElielCastroHernandez1207 <punkan1207@gmail.com>
2026-03-25 19:49:40 -0600

    Subiendo Codigos Completos

--- src/Problema_1/Cliente.java
+++ src/Problema_1/Cliente.java
@@ -1,4 +1,31 @@
 package Problema_1;
 
 public class Cliente {
+    private String nombre;
+    private String celular;
+    private String identificacion;
+
+    public Cliente(String nombre, String celular, String identificacion) {
+        this.nombre = nombre;
+        this.celular = celular;
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getCelular() {
+        return this.celular;
+    }
+
+    public String getIdentificacion() {
+        return this.identificacion;
+    }
+
+    public String toString() {
+        return this.nombre;
+    }
 }
+
+
